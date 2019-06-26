644b2bf741f192c64b4df4b80d401ebaabe24e64
Wallace Silva <wallace.silva@titcs.com.br>
2019-06-26 17:57:20 -0300

    Reactotron Conectado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
+
 import store from './store';
 import TodoList from './TodoList.js';
-import './config/ReactotronConfig';
 
 const App = () => (
   <Provider store={store}>
