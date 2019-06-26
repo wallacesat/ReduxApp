77b13752470630734d3f076dbc669a4557032900
Wallace Silva <wallace.silva@titcs.com.br>
2019-06-26 17:30:42 -0300

    Reactotron

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
-
 import TodoList from './TodoList.js';
+import './config/ReactotronConfig';
 
 const App = () => (
   <Provider store={store}>
