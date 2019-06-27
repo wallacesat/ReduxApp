4906c96805701e0ba4380f4a7c60b538ab674af7
Wallace Silva <wallace.silva@titcs.com.br>
2019-06-27 11:37:26 -0300

    Footer de contagem de favoritos conectado ao Redux

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,69 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
 
-const Main = () => <h1>Main</h1>;
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
 
-export default Main;
+class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string
+      })
+    ).isRequired
+  };
+
+  state = {
+    repository: ''
+  };
+
+  handleAddRepository = event => {
+    event.preventDefault();
+
+    this.props.addFavorite();
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repository}
+            onChange={e => this.setState({ repository: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          {this.props.favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name}</strong> ({favorite.description})
+              </p>
+              <a href={favorite.url}>Acessar</a>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Main from '../pages/main'
+import Main from '../pages/main';
+import Footer from '../components/Footer';
 
 const Routes = () => (
-<BrowserRouter>
-  <Switch>
-    <Route exact path="/" component={Main}/>
-  </Switch>
-</BrowserRouter>
+  <BrowserRouter>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+
+      <Footer />
+    </Fragment>
+  </BrowserRouter>
 );
 
 export default Routes;
