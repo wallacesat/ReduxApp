3efea9ac345969258e8dc0ae0b7b85d73db63777
Wallace Silva <wallace.silva@titcs.com.br>
2019-06-28 14:23:13 -0300

    Duck Pattern

---
+++ src/store/ducks/favorites.js
@@ -0,0 +1,54 @@
+/**
+ *  TYPES
+ */
+export const Types = {
+  ADD_REQUEST: 'favorites/ADD_REQUEST',
+  ADD_SUCCESS: 'favorites/ADD_SUCCESS',
+  ADD_FAILURE: 'favorites/ADD_FAILURE'
+};
+
+/**
+ *  REDUCERS
+ */
+const INITIAL_STATE = {
+  loading: false,
+  data: []
+};
+
+export default function favorites(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case Types.ADD_REQUEST:
+      return { ...state, loading: true, error: null };
+    case Types.ADD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data: [...state.data, action.payload.data]
+      };
+    case Types.ADD_FAILURE:
+      return { ...state, loading: false, error: action.payload.error };
+    default:
+      return state;
+  }
+}
+
+/**
+ *  ACTIONS
+ */
+export const Creators = {
+  addFavoriteRequest: repository => ({
+    type: Types.ADD_REQUEST,
+    payload: { repository }
+  }),
+
+  addFavoriteSuccess: data => ({
+    type: Types.ADD_SUCCESS,
+    payload: { data }
+  }),
+
+  addFavoriteFailure: error => ({
+    type: Types.ADD_FAILURE,
+    payload: { error }
+  })
+};
